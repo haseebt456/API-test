a1b69cd12861679043f4e4765ed3258e4ae0ed66
haseebt456 <SP22-BSE-075@CUILAHORE.EDU.PK>
2023-11-09 01:15:17 +0500

    delete button added

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ function loadRecipes() {
         method: "GET",
         success: function(response){
             console.log(response);
+            var recipe=$("#recipes");
+            recipe.empty();
+            for(let i=0 ;i<response.length;i++)
+            {
+                var rec = response[i];
+                recipe.append(`
+                <div class="recipe">
+                <h3>${rec.title}</h3>
+                <p>${rec.body}</p><button class="btn btn-danger btn-sm">delete</button>
+                </div>`);
+            }
         }
     })
 }
